c80db747ee2550da3d5aa389f1409c7fb5888292
Alejandro Polo <alejandrohernandezpolo@gmail.com>
2024-01-25 17:42:22 -0500

    Leggiamo 1.0

--- scripts.js
+++ scripts.js
@@ -1,140 +1,141 @@
-// Sets all the information from JS to HTML
-document.addEventListener("DOMContentLoaded", function() {
-  addHeader();
-  addFooter();
-});
+import meditations from './meditations.json' assert {type: "json"};
 
+// General Items
+const modeButton = document.querySelector('.mode');
+const body = document.querySelector('body');
+const header = document.querySelector('#header nav');
+const headerIcon = document.querySelector('#header i');
+const headerLinks = document.querySelectorAll('#header nav a');
+const footer = document.querySelector('#footer');
+const footerLinks = document.querySelectorAll('#footer i');
 
-// Adds the header and the footer
-function addHeader() {
-  const header = document.querySelector('#header');
-  header.innerHTML = `<div>
-                        <i class="fa-solid fa-book-open-reader"></i> 
-                        <p> Leggiamo </p>
-                      </div>
-                      <ul>
-                      <li> <a href=""> Terror </a> </li>
-                          <li> <a href=""> Misterio </a> </li>
-                          <li> <a href=""> Romance </a> </li>
-                          <li> <a href=""> Humor </a> </li>
-                      </ul>`;
+// Landing Items
+const main = document.querySelector('main .welcoming p');
+const mainTitle = document.querySelectorAll('main .content h2');
+const mainLinks = document.querySelectorAll('main .content a');
+
+// Story/Meditation Items
+const ad = document.querySelector('.advertisement');
+const CloseAd = document.querySelector('.close');
+const meditation = document.querySelector('.meditation');
+let lightMode = localStorage.getItem('lightMode') === 'true';
+
+// Advertisement
+if (!location.href.includes('index.html')) {
+  CloseAd.addEventListener('click', function() {
+    ad.style.display = 'none';
+  });
 }
 
-function addFooter() {
-  const footer = document.querySelector('#footer');
-  footer.innerHTML = `<div class="logo">
-                        <img src="images/GY.png" alt="">
-                      </div>
-                      <div class="copyright">    
-                        <h3> Derechos de autor </h3>
-                        <p> Todas las historias son proporcionadas por los autores descritos en la misma, mientras que las portadas de las historias son creadas por Leonardo AI. </p>
-                      </div>
-                      <div class="contact">
-                        <h3> Contáctanos </h3>
-                        <p> <i class="fa-solid fa-envelope"></i> jhalejandro@uninorte.edu.co</p>
-                        <p> <i class="fa-solid fa-phone"></i> +57 324 3168468 </p>
-                      </div>`;
+function applyColors() {
+  const darkColors = {
+    body: '#4A5759',
+    header: '#DEDBD2',
+    icon: '#4A5759',
+    links: '#4A5759',
+    buttonBg: '#DEDBD2',
+    buttonColor: '#4A5759',
+    footer: '#DEDBD2',
+    mainText: '#f3ccf9',
+    linkBg: '#f3ccf9',
+    linkColor: '#4A5759',
+    ad: '#DEDBD2'
+  };
+
+  const lightColors = {
+    body: '#DEDBD2',
+    header: '#4A5759',
+    icon: '#DEDBD2',
+    links: '#DEDBD2',
+    buttonBg: '#4A5759',
+    buttonColor: '#DEDBD2',
+    footer: '#4A5759',
+    mainText: '#BA8990',
+    linkBg: '#BA8990',
+    linkColor: '#F7E1D7',
+    ad: '#4A5759'
+  };
+
+  const colors = lightMode ? lightColors : darkColors;
+
+  body.style.backgroundColor = colors.body;
+  header.style.backgroundColor = colors.header;
+  headerIcon.style.color = colors.icon;
+  headerLinks.forEach(link => (link.style.color = colors.links));
+  modeButton.style.backgroundColor = colors.buttonBg;
+  modeButton.style.color = colors.buttonColor;
+  footer.style.color = colors.footer;
+  footerLinks.forEach(link => (link.style.color = colors.footer));
+
+  if (location.href.includes('index.html')) {
+    main.style.color = lightMode ? lightColors.mainText : darkColors.mainText;
+    mainTitle.forEach(title => (title.style.color = main.style.color));
+    mainLinks.forEach(link => {
+      link.style.backgroundColor = lightMode ? lightColors.linkBg : darkColors.linkBg;
+      link.style.color = lightMode ? lightColors.linkColor : darkColors.linkColor;
+    });
+  } else {
+    ad.style.backgroundColor = lightMode ? lightColors.linkBg : darkColors.linkBg;
+    ad.style.color = lightMode ? lightColors.linkColor : darkColors.linkColor;
+    meditation.style.color = lightMode ? lightColors.linkBg : darkColors.linkBg;
+  }
 }
 
-const imagen = document.querySelector('.enlace');
+function toggleDarkMode() {
+  // Toggle the mode
+  lightMode = !lightMode;
+
+  // Store the current light mode state in local storage
+  localStorage.setItem('lightMode', lightMode);
+
+  // Apply the colors
+  applyColors();
+}
+
+modeButton.addEventListener('click', toggleDarkMode);
+
+// Apply the colors when the page loads
+applyColors();
 
 // Gets the currently URL
-var urlActual = window.location.href;
+let urlActual = window.location.href;
 
 // Gets the query from the URL
-var cadenaConsulta = window.location.search;
+let cadenaConsulta = window.location.search;
 
 // Creates an object URLSearchParams from the query string
-var parametros = new URLSearchParams(cadenaConsulta);
+let parametros = new URLSearchParams(cadenaConsulta);
 
 // Obtain the value
-var title = parametros.get("title");
+let title = parametros.get("title");
+addInformation(title, meditations);
 
-var info1 = {
-  title: "Odio la ciudad",
-  author: "Nicolás Lara",
-  text: "historias/nicolas-lara.txt",
-  imgUrl: "nicolas-lara.png",
-  description: "descripciones/nicolas-lara.txt"
-};
-var info2 = {
-  title: "Solo queda escapar",
-  author: "Nathalia Sánchez",
-  text: "historias/nathalia-sanchez.txt",
-  imgUrl: "nathalia-sanchez.png",
-  description: "descripciones/nathalia-sanchez.txt"
-};
-var info3 = {
-  title: "El trabajo es mi pasión",
-  author: "Andrés García Acuña",
-  text: "historias/andres-garcia.txt",
-  imgUrl: "no-photo.png",
-  description: "descripciones/andres-garcia.txt"
-};
-var info4 = {
-  title: "Razas encontradas",
-  author: "Javier Pizarro",
-  text: "historias/javier-pizarro.txt",
-  imgUrl: "no-photo.png",
-  description: "descripciones/javier-pizarro.txt"
-};
-var info5 = {
-  title: "Aún me llaman señor",
-  author: "Jesús Cristancho",
-  text: "historias/jesus-cristancho.txt",
-  imgUrl: "no-photo.png",
-  description: "descripciones/jesus-cristancho.txt"
-};
-var infos = [info1, info2, info3, info4, info5];
-addInformation(title, infos);
-
-function addInformation(title, infos){
+function addInformation(title, meditations){
 
   // Runs across the Information List
-  infos.forEach(function(dict) {
+  for (let meditation in meditations) {
+    // console.log('entra');
     
-
     // Checks the comparison between story titles
-    if (title == dict.title) {
-
-      document.title = dict.title;
-
-      // Story 
-
-      // Searches the correct .txt
-
-      getDataFromTxt(dict.text, '.text-description');
+    if (title == meditations[meditation]["title"]) {
+      // console.log('entra if');
+      
+      document.title = meditations[meditation]["title"];
+      getDataFromTxt(meditations[meditation]["text"], '.text-description');
       
       // Injects the HTML code using the dictionary
       const story = document.querySelector('#story');
-      story.innerHTML = `<h2> ${dict.title} </h2>
+      story.innerHTML = `<h2> ${meditations[meditation]["title"]} </h2> <br>
                         <p class="text-description"> </p>`;
-
-      // Description of the Author
-
-      getDataFromTxt(dict.description, '.description');
-
-      const author = document.querySelector('#author');
-      author.innerHTML = `<img src="images/${dict.imgUrl}" alt="">
-                          <h3> ${dict.author} </h3>
-                          <p class="description"> </p>
-                          <div>
-                              <i class="fa-brands fa-instagram"></i>
-                              <i class="fa-brands fa-facebook"></i>
-                              <i class="fa-solid fa-envelope"></i>
-                          </div>`;
     }
-    return
-  });
+    // return
+  }
 }
 
 function getDataFromTxt(path, className) {
   const xhttp = new XMLHttpRequest();
-  
   xhttp.open('GET', path, true);
-
   xhttp.send();
-
   xhttp.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
       const texto = document.querySelector(className);
@@ -142,7 +143,3 @@ function getDataFromTxt(path, className) {
     }
   }
 }
-
-  /*var txt = txt;
-  var imgUrl = imgUrl;
-  var selfDescription = selfDescription;*/
