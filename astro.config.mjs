8c329bef602f1eb94399d4fb75b9d0921cbecb7e
adechlien <jhalejandro@uninorte.edu.co>
2026-02-07 16:04:22 -0500

    trying netlify option instead of vercel 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,20 +2,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/serverless";
-import decapCmsOauth from "astro-decap-cms-oauth";
 
 export default defineConfig({
   site: "https://adechlien.blog",
-  adapter: vercel(),
   output: "server",
   integrations: [
     mdx(),
     sitemap(),
     tailwind(),
-    decapCmsOauth({
-      baseUrl: "https://adechlien.blog",
-      authPath: "/api/auth",
-    }),
   ],
 });
