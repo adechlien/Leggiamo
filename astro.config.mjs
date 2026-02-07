d8622e732651e405a50ece822823ba650a12cf61
adechlien <jhalejandro@uninorte.edu.co>
2026-02-07 12:13:17 -0500

    adding adapter for ssr

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,13 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
+import vercel from "@astrojs/vercel/serverless";
 import decapCmsOauth from "astro-decap-cms-oauth";
 
 export default defineConfig({
   site: "https://adechlien.blog",
+  adapter: vercel(),
+  output: "server",
   integrations: [
     mdx(),
     sitemap(),
