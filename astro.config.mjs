8975f149fe6a2c85e76aa47b409bc940d52af602
adechlien <jhalejandro@uninorte.edu.co>
2026-02-07 11:53:47 -0500

    fixed dependency issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,18 @@
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
-import decapCmsOauth from 'astro-decap-cms-oauth';
+import decapCmsOauth from "astro-decap-cms-oauth";
 
-// import db from "@astrojs/db";
-
-// https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
-  integrations: [mdx(), sitemap(), tailwind(), decapCmsOauth()]
+  site: "https://adechlien.blog",
+  integrations: [
+    mdx(),
+    sitemap(),
+    tailwind(),
+    decapCmsOauth({
+      baseUrl: "https://adechlien.blog",
+      authPath: "/api/auth",
+    }),
+  ],
 });
